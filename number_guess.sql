2d37a918f79b83fafcd5f03ae134ffef82e1d9d5
nicholaslpollard <nicholaslpollard@gmail.com>
2025-04-16 11:25:24 -0400

    fix: fix logic

--- number_guess.sql
+++ number_guess.sql
@@ -5,6 +5,7 @@
 -- Dumped from database version 12.17 (Ubuntu 12.17-1.pgdg22.04+1)
 -- Dumped by pg_dump version 12.17 (Ubuntu 12.17-1.pgdg22.04+1)
 
+
 SET statement_timeout = 0;
 SET lock_timeout = 0;
 SET idle_in_transaction_session_timeout = 0;
